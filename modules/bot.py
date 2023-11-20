eec2ca7be740f4f828c6c696cb79f41a63cd251f
Huguitis <mail@huguitisnodes.host>
2023-11-20 23:57:40 +0100

    Update bot.py

--- modules/bot.py
+++ modules/bot.py
@@ -143,7 +143,7 @@ class Bot:
         self.use_device_auth = use_device_auth
         self.use_authorization_code = use_authorization_code
 
-        self.input_lock = asyncio.Lock()
+        self.input_lock = asyncio.Lock(loop=loop)
         self.clients = []
         self.updater = Updater(self)
         self.web = Web(self, 'modules_bot')
